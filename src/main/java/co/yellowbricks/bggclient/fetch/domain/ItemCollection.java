52d076748eafd94456e80a8792a1c71b8ae5b897
Marcio Oliveira <marciottlg@gmail.com>
2013-07-20 23:18:44 -0300

    Adding equals and hashcode to ItemCollection

--- src/main/java/co/yellowbricks/bggclient/fetch/domain/ItemCollection.java
+++ src/main/java/co/yellowbricks/bggclient/fetch/domain/ItemCollection.java
@@ -6,6 +6,8 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.base.Objects;
+
 @XmlRootElement(name = "items")
 public class ItemCollection {
 
@@ -28,5 +30,21 @@ public class ItemCollection {
 	
 	public List<CollectionItem> getItems() {
 		return items;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(totalItems, termsOfUseUrl, items);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ItemCollection) {
+			ItemCollection that = (ItemCollection) obj;
+			return Objects.equal(totalItems, that.totalItems)
+					&& Objects.equal(termsOfUseUrl, that.termsOfUseUrl)
+					&& Objects.equal(items, that.items);
+		}
+		return false;
 	}
 }
