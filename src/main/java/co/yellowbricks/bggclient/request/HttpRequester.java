4bbab4a7c8a9ce8a72d83bc2e0503af629840039
Marcio Oliveira <marciottlg@gmail.com>
2013-12-28 20:43:21 -0200

    Increasing request timeout

--- src/main/java/co/yellowbricks/bggclient/request/HttpRequester.java
+++ src/main/java/co/yellowbricks/bggclient/request/HttpRequester.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder;
 import com.ning.http.client.Response;
 
 public enum HttpRequester {
-    
+
     INSTANCE;
 
     public InputStream executeRequest(String url, ParameterAdder parameterAdder) throws BggServiceException {
@@ -21,20 +23,22 @@ public enum HttpRequester {
 			httpClient.close();
 		}
 	}
-	
+
 	private InputStream executeRequest(BoundRequestBuilder request) throws BggServiceException {
 		try {
 			Future<Response> future = request.execute();
-			return future.get().getResponseBodyAsStream();
+			return future.get(3l, TimeUnit.MINUTES).getResponseBodyAsStream();
 		} catch (InterruptedException e) {
 			throw createException(request, e);
 		} catch (ExecutionException e) {
 			throw createException(request, e);
 		} catch (IOException e) {
 			throw createException(request, e);
-		}
+		} catch (TimeoutException e) {
+            throw createException(request, e);
+        }
 	}
-	
+
 	private BggServiceException createException(BoundRequestBuilder request, Throwable e) {
 		return new BggServiceException(String.format("While processing request %s", request.build()), e);
 	}
